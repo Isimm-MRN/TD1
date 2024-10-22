281d9f24a57aa873398bbdcbf7560e146d730c45
Isimm-MRN <formation.latex.2021@gmail.com>
2024-10-22 09:26:06 +0100

    Update Td1.py

--- Td1.py
+++ Td1.py
@@ -1,36 +1,26 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt 
-%matplotlib inline
 np.random.seed(5)
 mean1 = [0, 0]
 mean2 = [1, 5]
 cov1 = [[1, 0.9], [0.9, 1]]  # diagonal covariance
 cov2 = [[1, 0.75], [0.75, 1]]
 import matplotlib.pyplot as plt
-
 X1 = np.random.multivariate_normal(mean1, cov1, 5)
 X2 = np.random.multivariate_normal(mean2, cov2, 5)
 plt.plot(X1[:,0], X1[:,1], 'o')
 plt.plot(X2[:,0], X2[:,1], 'o')
 #plt.axis('equal')
-
 plt.show()
-
 # %%
 X=np.vstack((X1,X2))
-X
-
 # %%
 l1 = np.ones((5,1))
 l2= -np.ones((5,1))
 l=np.vstack((l1,l2))
-l
-
 # %%
 dataset = np.hstack((X,l))
-dataset
-
 # %%
 # shuffling the data to make the sampling random
 np.random.shuffle(dataset)
@@ -38,9 +28,3 @@ m=4
 # splitting the data into train/test sets
 train = dataset[0:7]
 test = dataset[7:10]
-train
-
-# %%
-test
-
-
