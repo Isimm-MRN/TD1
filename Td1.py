c2a587b9f4ba6ef297daa0598377e7d8b091ee9d
Isimm-MRN <formation.latex.2021@gmail.com>
2024-10-22 09:33:09 +0100

    Update Td1.py

--- Td1.py
+++ Td1.py
@@ -6,9 +6,9 @@ mean1 = [0, 0]
 mean2 = [1, 5]
 cov1 = [[1, 0.9], [0.9, 1]]  
 cov2 = [[1, 0.75], [0.75, 1]]
-import matplotlib.pyplot as plt
-X1 = np.random.multivariate_normal(mean1, cov1, 5)
-X2 = np.random.multivariate_normal(mean2, cov2, 5)
+m=10
+X1 = np.random.multivariate_normal(mean1, cov1, m)
+X2 = np.random.multivariate_normal(mean2, cov2, m)
 plt.plot(X1[:,0], X1[:,1], 'o')
 plt.plot(X2[:,0], X2[:,1], 'o')
 #plt.axis('equal')
@@ -16,8 +16,8 @@ plt.show()
 # %%
 X=np.vstack((X1,X2))
 # %%
-l1 = np.ones((5,1))
-l2= -np.ones((5,1))
+l1 = np.ones((m,1))
+l2= -np.ones((m,1))
 L=np.vstack((l1,l2))
 # %%
 dataset = np.hstack((X,L))
